e0da68d8fdfad4846dee8592bde831f31a435629
Joe Burdick <josephdburdick@gmail.com>
2015-05-11 17:27:38 -0400

    Attempt to wire click events to one infowindow from many markers.

--- app/client/templates/shared/nearby_map/nearby_map.js
+++ app/client/templates/shared/nearby_map/nearby_map.js
@@ -27,37 +27,77 @@ Template.NearbyMap.helpers({
 Template.NearbyMap.created = function () {
 	// getUserGeolocation();
   GoogleMaps.ready('map', function(map) {
+    
+    var infowindow = new google.maps.InfoWindow(),
+        marker = new google.maps.Marker();
 
   	Venues.find().forEach(function(doc){
-  		var infowindow = new google.maps.InfoWindow({
-			    content: [
-			    	'<header>' + doc.location.name.capitalizeFirstLetter() + '</header>',
-			    	'<main>' + doc.location.address.capitalizeFirstLetter() + '</main>'].join('')
-			});
-			var marker = new google.maps.Marker({
-	      position: new google.maps.LatLng(doc.location.lat, doc.location.lon),
-	      map: map.instance
-	    });
-	    marker.set('title', doc.location.name);
-	    marker.addListener('click',function(){
-        window.location.hash = '#t_' + doc._id;
-        infowindow.open(map.instance, marker);
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(doc.location.lat, doc.location.lon),
+        map: map.instance,
+        title: doc.location.name.toTitleCase()
       });
-   //    google.maps.event.addListener(marker, 'click', function() {
-			  
-			// });		
-		});
+      marker.location = {
+        name: doc.location.name,
+        address: doc.location.address,
+      };
+    });
 
-		var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(Session.get('lat'), Session.get('lon')),
-      icon: 'http://maps.gstatic.com/mapfiles/markers2/icon_green.png',
-      map: map.instance,
-      title: "Your Geolocation"
-    });		
+    // marker.addListener('click',function(){
+    //   window.location.hash = '#t_' + doc._id;
+    //   // debugger;
+    //   infowindow.set(map.instance, marker);
+    //   // google.maps.event.addListener(marker, 'click', function(e) {
+    //   infowindow.setContent(
+    //     [ '<header>' + this.location.name.toTitleCase() + '</header>',
+    //       '<main>' + this.location.address.toTitleCase() + '</main>'
+    //     ].join('')
+    //   );
+    //   infowindow.open(map.instance, this);
+    //   // });
+    //});
+
+    google.maps.event.addListener(marker, 'click', function(){
+      alert(666);
+    });
+    // map.event.addListener(marker, 'click', function() {
+    //   infowindow.open(map,marker);
+    //   console.log(777);
+    // });
+  	// 	infowindow = new google.maps.InfoWindow({
+			//     content: [
+			//     	'<header>' + doc.location.name.toTitleCase() + '</header>',
+			//     	'<main>' + doc.location.address.toTitleCase() + '</main>'].join('')
+			// });
+			// marker = new google.maps.Marker({
+	  //     position: new google.maps.LatLng(doc.location.lat, doc.location.lon),
+	  //     map: map.instance
+	  //   });
+	  //   // marker.set('title', doc.location.name);
+
+	  //   marker.addListener('click',function(){
+   //      window.location.hash = '#t_' + doc._id;
+   //      infowindow.open(map.instance, marker);
+   //    });
+		// });
+    // google.maps.event.addListener(marker, 'click', function() {
+    //   infowindow.open(map,marker);
+    // });
+		// marker = new google.maps.Marker({
+  //     position: new google.maps.LatLng(Session.get('lat'), Session.get('lon')),
+  //     icon: 'http://maps.gstatic.com/mapfiles/markers2/icon_green.png',
+  //     map: map.instance,
+  //   });
 		
 		$(window).on('resize', function(){
 			map.instance.setCenter(new google.maps.LatLng(Session.get('lat'), Session.get('lon')));
 		});
+
+    // google.maps.event.addListener(marker, 'click', function(e) {
+    //   console.log(e);
+    //   infowindow.setContent('Hello World');
+    //   infowindow.open(map.instance, this);
+    // });
 
   });
 };
